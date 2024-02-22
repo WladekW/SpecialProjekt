0975159ac8c5f9cd4147b07700537180710e868f
Władysław Berdzik <146719514+WladekW@users.noreply.github.com>
2024-02-22 19:18:18 +0100

    Update menu.gd

--- menu.gd
+++ menu.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
-
+var menu = true
 
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://ustawienia.tscn")
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://level_1.tscn")
